1586a34024bc973a34a4f01b9ce5f6f1f3b2d81d
EyobaGit <juu.shashe@gmail.com>
2025-07-07 18:35:03 +0300

    updated

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const axiosInstance = axios.create({
   // baseURL: "http://127.0.0.1:5001/clone-e752e/us-central1/api",
-  
+
   //firebase
   // baseURL: "https://api-e6cratvzyq-uc.a.run.app/",
   baseURL:"https://amazon-api-5.onrender.com"
